ec188670a9e60a3e56691ed42d7305306e52fe2a
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-03-02 18:10:55 +0000

    Automatically backed up by Learn

--- app/serializers/sighting_serializer.rb
+++ app/serializers/sighting_serializer.rb
@@ -1,10 +1,6 @@
 class SightingSerializer
-  def show
-  sighting = Sighting.find_by(id: params[:id])
-  options = {
-    include: [:bird, :location]
-  }
-  render json: SightingSerializer.new(sighting, options)
-end
-
+  include FastJsonapi::ObjectSerializer
+  attributes :created_at
+  belongs_to :bird
+  belongs_to :location
 end
